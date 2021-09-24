2131776241998d6594ad3f37eb312eb95b75bcd3
Amie Foster <amie.n.foster@gmail.com>
2021-09-23 22:27:52 -0600

    finished!!!!!!!

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useState } from "react"
 
 function Item({ name, category }) {
+
+  /// ADD TO CART DELIVERABLE \/\/\/\/
+  const [addCart, setAddCart] = useState(true)
+  const handleAddCart = () => {
+    setAddCart(!addCart)
+  }
+  // \/\/\/these are all changed by toggling addCart in the handleAddCart function \/\/\/
+  const addedCartClass = addCart ? " " : "in-cart" //changes class from nothing to in cart
+  const addCartButtonClass = addCart ? "add" : "remove" //changes class on button from add to remove
+  const addCartButtonText = addCart ? "Add to Cart" : "Remove From Cart" //changes text on button
+  /// ADD TO CART DELIVERABLE /\/\/\/\
+
+
   return (
-    <li className="">
+    <li onClick={handleAddCart} className={addedCartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addCartButtonClass}>{addCartButtonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react"
 
 function ShoppingList({ items }) {
+
+  ///FILTER DELIVERABLE \/\/\/\/
+  const [selectedCategory, setSelectedCategory] = useState("All") //initial value is All
+
+  const handleSelectCategory = (event) => {
+    setSelectedCategory(event.target.value)
+  }
+  
+  const categoryToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  })
+  console.log(categoryToDisplay)
+
+  let renderItems = selectedCategory ? categoryToDisplay : items
+  //changes what is being mapped over and rendered into the li from the specified category to the items - if no category is selected it will default to items, if a category is selected it will map over that array with corosponding category and only render items that match!!
+
+  // const renderItems = selectedCategory ? categoryToDisplay : 
+  ///FILTER DELIVERABLE /\/\/\/\
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleSelectCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +37,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {renderItems.map((item) => (<Item key={item.id} name={item.name} category={item.category} />))}
       </ul>
     </div>
   );
