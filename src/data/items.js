421c8c1392424c6bc2da7c5cd7e6769b994e5dba
Amie Foster <amie.n.foster@gmail.com>
2021-09-23 22:33:20 -0600

    added more items to the list for fun

--- src/data/items.js
+++ src/data/items.js
@@ -3,7 +3,15 @@ const items = [
   { id: 2, name: "Pomegranate", category: "Produce" },
   { id: 3, name: "Lettuce", category: "Produce" },
   { id: 4, name: "String Cheese", category: "Dairy" },
-  { id: 5, name: "Cookies", category: "Dessert" },
+  { id: 5, name: "Cake", category: "Dessert" },
+  { id: 6, name: "Pumpkin Pie", category: "Dessert" },
+  { id: 7, name: "Carrots", category: "Produce" },
+  { id: 8, name: "Celery", category: "Produce" },
+  { id: 9, name: "Coffee Creamer", category: "Dairy" },
+  { id: 10, name: "Butter", category: "Dairy" },
+  { id: 11, name: "Jell-o", category: "Dessert" },
+  { id: 12, name: "Peaches", category: "Produce" },
+  { id: 13, name: "Cookies", category: "Dessert" },
 ];
 
 export default items;
